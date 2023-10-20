c81471d539d18281eeec048bf2d1aa163ecf28ab
FarooqProProgrammer <farooqayub646@gmail.com>
2023-10-20 15:37:25 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Header from './components/Header'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Pricing from './pages/Pricing'
+import Footer from './components/Footer'
+import Blog from './pages/Blog'
+import SingleBlog from './pages/SingleBlog'
 
 export default function App() {
   return (
@@ -10,7 +14,12 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' Component={Home} />
+            <Route path='/pricing' Component={Pricing} />
+            <Route path='/blog' Component={Blog} />
+            <Route path='/single' Component={SingleBlog} />
+            
           </Routes>
+          <Footer />
         </BrowserRouter>
     </>
   )
